7e70caed97cc0f9f7db35499ef768ffe562ee40b
jackiejou <joujackie@gmail.com>
2017-12-20 17:43:49 -0800

    Good but disgusted - Finished POST and downloadUrls

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
-
+var https = require('https');
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
  * Consider using the `paths` object below to store frequently used file paths. This way,
@@ -11,6 +11,8 @@ var _ = require('underscore');
 
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
+  publicIndex: path.join(__dirname, '../web/public/index.html'),
+  publicLoading: path.join(__dirname, '../web/public/loading.html'),
   archivedSites: path.join(__dirname, '../archives/sites'),
   list: path.join(__dirname, '../archives/sites.txt')
 };
@@ -25,17 +27,62 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
+// Worker Only
 exports.readListOfUrls = function(callback) {
+  // read file
+  // parse to array
+  // callback(parsedArray)
+  fs.readFile(exports.paths.list, (err, data) => {
+    callback(data);
+  });
 };
 
+// Not sure
 exports.isUrlInList = function(url, callback) {
 };
 
-exports.addUrlToList = function(url, callback) {
+// Server Only
+exports.addUrlToList = function(url) {
+  fs.appendFile(exports.paths.list, url + '\n', () => {
+    console.log('Appended url!');
+  });
 };
 
-exports.isUrlArchived = function(url, callback) {
+// Server Only
+exports.isUrlArchived = function(url, trueCallback, falseCallback) {
+
+  // check if url is in /archives/sites/
+  // if it is, trueCallback(url);
+
+  falseCallback(url);
+
 };
 
-exports.downloadUrls = function(urls) {
+// Worker
+exports.downloadUrls = function(url) {
+  https.get('https://' + url, (res) => {
+    html = '';
+    res.on('data', chunk => {
+      html += chunk;
+    });
+
+    res.on('end', () => {
+      let location = path.join(exports.paths.archivedSites, url + '.html');
+      console.log(location);
+      fs.writeFile(location, html, () => {
+        console.log('Wrote file to', location);
+      });
+    });
+  });
+};
+
+// Worker
+exports.removeFirstUrl = function() {
+// WORKER
+// string = readFile
+// var array = string.split('\n')
+// var site = array.shift()
+// worker(site)
+// var file = array.join('/n');
+// writeFile(file)
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,70 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var httpHelpers = require('./http-helpers');
+var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  
+  if (req.method === 'GET') {
+    httpHelpers.serveAssets(res, archive.paths.publicIndex, (data) => {
+      res.end(data);
+    });
+  }
+  
+  // get url from POST request
+  if (req.method === 'POST') {
+    url = '';
+    req.on('data', chunk => {
+      url += chunk;
+    });
+    req.on('end', () => {
+      url = url.split('=')[1];
+      console.log('You posted', url);
+      archive.isUrlArchived(url, (url) => {
+        // true
+        // TODO
+      }, (url) => {
+        // false
+        httpHelpers.serveAssets(res, archive.paths.publicLoading, (data) => {
+          res.end(data);
+        });
+        archive.addUrlToList(url);
+      });
+      // is Url Archived
+          // callback: 
+            // if present, do x
+            // if not present do y
+              // callback: 
+                // return publicLoading
+                // write url to sites file
+
+
+    });
+
+                     
+
+
+
+  }
+
+  
+
+  //res.end(JSON.stringify(archive.paths));
 };
+
+
+// exports.readListOfUrls = function(callback) {
+// };
+
+// exports.isUrlInList = function(url, trueCallback, falseCallback) {
+// };
+
+// exports.addUrlToList = function(url) {
+// };
+
+// exports.isUrlArchived = function(url, callback) {
+// };
+
+// exports.downloadUrls = function(url) {
+// };
